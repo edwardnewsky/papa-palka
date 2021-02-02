eb11f1c4e38a9af001646eb801cd716e2755319d
EdwardNewsky <EdwardNewsky@gmail.com>
2021-02-02 15:00:24 +0300

    Start

---
+++ js/script.js
@@ -0,0 +1,11 @@
+var mySwiper = new Swiper('.swiper-container', {
+  // Optional parameters
+  slidesPerView: 1,
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
